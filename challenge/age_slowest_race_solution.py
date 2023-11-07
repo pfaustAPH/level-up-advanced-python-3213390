642a95f38c1c29606d79f9c9d51879880d18862b
Paul Faust <121193313+pfaustAPH@users.noreply.github.com>
2023-11-07 21:31:46 +0000

    test commit

--- challenge/age_slowest_race_solution.py
+++ challenge/age_slowest_race_solution.py
@@ -1,6 +1,7 @@
 # Source of data: https://www.arrs.run/
 # This dataset has race times for women 10k runners from the Association of Road Racing Statisticians
 # Assume a year has 365.25 days
+# This is a test of the codespace
 
 import re
 import datetime
